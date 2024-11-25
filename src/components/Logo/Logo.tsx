a6d6dfe29d9836fe52e3ac640b05a615c245fce3
AKKK1 <akakichachava1@gmail.com>
2024-11-25 19:15:05 +0100

    asd

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx'
 import React from 'react'
-
+import logo from '../../public/lg.png'
 interface Props {
   className?: string
   loading?: 'lazy' | 'eager'
@@ -23,7 +23,7 @@ export const Logo = (props: Props) => {
       fetchPriority={priority}
       decoding="async"
       className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+     src="/LGB.png"
     />
   )
 }
